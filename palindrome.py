51c75b8ec447e1d7942c9ccebcf60c1793e9a59b
swathipoojary99 <98868484+swathipoojary99@users.noreply.github.com>
2022-03-17 10:28:42 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,6 @@
+x=input("Enter any sequence")
+y=x[::-1]
+if x==y:
+    print("PALINDROME")
+else:
+    print("NOT A PALINDROME")
---
+++ pattern.py
@@ -0,0 +1,4 @@
+def new(a):
+    for x in range(a):
+        print(''*(a-x-1)+'*'*(2*x+1))
+new(5)
