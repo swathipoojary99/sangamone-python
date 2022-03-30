50e775e078e1cc737f6c6b67f70ede2b1a4796a4
swathipoojary99 <98868484+swathipoojary99@users.noreply.github.com>
2022-03-30 10:30:58 +0530

    Add files via upload

---
+++ decimal.py
@@ -0,0 +1,9 @@
+decimal = int(input("Enter a decimal number \n"))
+binary = 0
+ctr = 0
+temp = decimal  #copy input decimal
+while(temp > 0):
+    binary = ((temp%2)*(10**ctr)) + binary
+    temp = int(temp/2)
+    ctr += 1      
+print("Binary of {x} is: {y}".format(x=decimal,y=binary))
