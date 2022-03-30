8a927b08fe2826a45d6e029677dbf6514ade6673
swathipoojary99 <98868484+swathipoojary99@users.noreply.github.com>
2022-03-30 09:57:39 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -2,7 +2,7 @@ def permute(s, answer):
     if (len(s) == 0):
         print(answer, end = "  ")
         return
-    for i in range(len(s))
+    for i in range(len(s)):
         ch = s[i]
         left_substr = s[0:i]
         right_substr = s[i + 1:]
